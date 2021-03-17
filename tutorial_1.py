37177f7462a343548f03467e39b7b9573cc0bfca
azukhrufy <azukhrufy@outlook.com>
2021-03-17 14:46:28 +0700

    feat(init): add html template and bootstrap to project

--- tutorial_1.py
+++ tutorial_1.py
@@ -2,11 +2,15 @@ from flask import Flask, redirect,url_for, render_template
 
 app = Flask(__name__)
 
-list = ["Budi", "Hartono", "Eky", "Zahir"]
+list = ["Budi", "Hartono", "Eky", "Zahir", "Kokom"]
 
 @app.route("/")
 def home():
     return render_template("index.html", name = list)
+
+@app.route("/test")
+def test():
+    return render_template("new.html")
 
 @app.route("/admin")
 def admin():
@@ -16,5 +20,6 @@ def admin():
 def user(name):
     return "Hello {}".format(name)
 
+    
 if __name__ == "__main__" :
     app.run()
